e99471631db49b9b4d41a8f6046fd8129f8e5d5f
Edward Newell <edward.newell@gmail.com>
2017-07-17 13:49:25 -0400

    require higher version of iterable-queue (==1.2.1)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 	# single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.1',
+    version='0.2.2',
 
     description='word2vec using Theano and Lasagne',
     long_description=long_description,
@@ -69,7 +69,7 @@ setup(
 		'word2vec': ['README.md']
 	},
 	install_requires=[
-		'numpy', 'scipy', 'theano', 'lasagne', 'iterable-queue', 
+		'numpy', 'scipy', 'theano', 'lasagne', 'iterable-queue==1.2.1', 
 		'categorical'
 	]
 )
